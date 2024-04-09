dd600f56403619e1043847b50019bc2de4da5949
ROSS DAVIDSON <r.davidson37@rgu.ac.uk>
2024-04-09 12:02:29 +0100

    com

---
+++ Assets/Scripts/AxeCollision.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeCollision : MonoBehaviour
+{
+    public int pointsPerHit = 1; // Points to gain per hit
+
+    // OnTriggerEnter is called when the Collider other enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Target")) // Assuming the target has a tag "Target"
+        {
+            // Increment points
+            GameManager.instance.AddPoints(pointsPerHit);
+            Debug.Log("Target Hit");
+
+            // Optionally, you can add sound or visual feedback here
+
+            // Destroy the axe
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; // Singleton instance
+
+    public TextMeshProUGUI scoreText; // Reference to the UI text displaying the score
+    private int score = 0; // Current score
+
+    private void Awake()
+    {
+        // Ensure only one instance of GameManager exists
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Method to add points to the score
+    public void AddPoints(int points)
+    {
+        score += points;
+        UpdateScoreUI();
+    }
+
+    //Method to update the score displayed in the UI
+    void UpdateScoreUI()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
